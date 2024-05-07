e6eb3bd91f115797d137519ea3ced3514acfc25f
gabe.c423 <102372882+Killer-kidGC@users.noreply.github.com>
2024-05-07 16:22:26 +0100

    Version 7, now has borders where the numbers will be placed as promts for where and what to fill in, the window now also has a name

---
+++ NonogramV7/Border.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Border extends JFrame {
+    
+    public Border(){
+
+        BorderLayout NumBorder = new BorderLayout();
+        setLayout(NumBorder);
+
+
+
+    }
+    
+}
---
+++ NonogramV7/Grid.java
@@ -0,0 +1,120 @@
+//Libraries for this class.
+import java.awt.*;
+import javax.swing.*;
+
+public class Grid{
+    //Grid attributes:
+    JButton[][] tiles;
+    JPanel gridframe;
+    int buttonXAxis;
+    int buttonYAxis;
+    int[][]clickCount;
+    int xAxis = 15;
+    int yAxis = 15;
+
+    //Constructor for the 'Grid' class.
+    public Grid() {
+
+        JFrame borderframe = new JFrame();
+        borderframe.setLayout(new BorderLayout());
+
+        tiles = new JButton[xAxis][yAxis];
+        gridframe = new JPanel();
+        borderframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Sets dimensions, row/column count and square spacing.
+        borderframe.setSize(900,1000);
+        borderframe.setTitle("Nonogram");
+        gridframe.setLayout(new GridLayout(15,15,10,10));
+
+        JPanel leftPanel = new JPanel();
+        leftPanel.setBackground(Color.gray);
+        leftPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 20));
+
+        JPanel topPanel = new JPanel();
+        topPanel.setBackground(Color.gray);
+        topPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 20));
+
+        //Grid represented as a 2D array of interactive buttons.
+        JButton[][] grid = getTileArray();
+        clickCount = new int[15][15];
+    
+        //A nested for loop within a for loop to create buttons for each square in each dimension of the array.
+        //The first loop being for the first dimension.
+        for (int i = 0; i < grid.length; i++ ){
+
+            //The nested loop being for the second dimension.
+            for (int j = 0; j < grid[0].length; j++){
+                
+                grid[i][j] = new JButton();
+                grid[i][j].setBackground(Color.GRAY);
+                grid[i][j].setSize(new Dimension(10, 10));
+                gridframe.add(grid[i][j]);
+                
+                //Sets an array to hold the amount of times a button has been clicked for each button.
+                clickCount[i][j] = -1;
+                
+            }
+
+            borderframe.add(gridframe, BorderLayout.CENTER);
+
+            borderframe.add(leftPanel, BorderLayout.WEST);
+
+            borderframe.add(topPanel, BorderLayout.NORTH);
+
+        }
+
+
+
+        borderframe.setVisible(true);
+
+    }
+
+
+
+
+    //Getter for returning the attribute 'gridframe' which is where the value for the playing grid is stored.
+    public JPanel getGridFrame() {
+
+        return gridframe;
+    }
+
+
+    //Getter method for returning the array of interactive tiles.
+    public JButton[][] getTileArray(){
+
+        return tiles;
+    }
+    
+
+    //Sets the value of the attribute 'buttonXAxis' to the current clicked button.
+    public void setXAxis(int x){
+
+        this.buttonXAxis = x;
+
+    }
+
+
+    //Sets the value of the attribute 'buttonYAxis' to the current clicked button.
+    public void setYAxis(int y){
+
+        buttonYAxis = y;
+
+    }
+
+
+    //gets the x-coordinate of clicked button.
+    public int getXAxis(){
+
+        return buttonXAxis;
+
+    }
+
+
+    //Gets the y-coordinate of clicked button.
+    public int getYAxis(){
+
+        return buttonYAxis;
+
+    }
+
+}
