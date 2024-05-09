17ce33ea6e8e474c4aa6ae03eb41ce07a8815ff4
gabe.c423 <102372882+Killer-kidGC@users.noreply.github.com>
2024-05-09 15:08:07 +0100

    Now displays numebers on side of grid

---
+++ NonogramV8/Main.java
@@ -0,0 +1,21 @@
+
+
+public class Main {
+    
+    //private ButtonReact buttonR;
+    public CheckSquare solution;
+    //private CheckSquare solution;
+
+    public void main(String[] args) {
+    
+        //buttonR = new ButtonReact();
+        solution = new CheckSquare();
+
+
+        
+
+
+
+        
+    }
+}
